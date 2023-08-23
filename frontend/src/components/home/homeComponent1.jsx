146ce3bae38ff3a496a67c0030d471a9f54c4e4a
vasiuddin7733 <vasiuddin7733>
2023-08-23 09:10:04 +0200

    add home intro component and its css files

--- frontend/src/components/home/homeComponent1.jsx
+++ frontend/src/components/home/homeComponent1.jsx
@@ -1,22 +1,35 @@
 import React from "react";
+import "../../styles/home/homeComponent1.css";
 
 const HomeComponent1 = () => {
   return (
-    <div>
-      <div>
-        <div className="flex justify-center items-center">
-          <div className="flex flex-col justify-center items-center h-96">
-            <div>Online Selbsthilfegruppen</div>
-            <div>
+    <div className="intro1">
+      <div className="intro2">
+        <div className="intro3">
+          <div className="intro4">
+            <div className="introtitle">Online Selbsthilfegruppen</div>
+            <div className="introcontent">
               Tausche Dich mit anderen Betroffenen aus oder starte eine neue
               Gruppe. Unabhängig von deinem Wohnort und passend zu deiner
               Lebenssituation.
             </div>
+            <div className="">
+              <button className="introbutton">Jetzt starten</button>
+            </div>
           </div>
-          <div>
-            <div className="#ACD7EB"></div>
-            <div></div>
-            <div></div>
+          <div className="introbild">
+            <div>
+              <div className="introcol"></div>
+              <div className="img1">
+                <img src="/home/intro-frau1.png"></img>
+              </div>
+              <div className="img2">
+                <img src="/home/intro-frau2.png"></img>
+              </div>
+            </div>
+            <div className="img2">
+              <img src="/home/intro-mann1.png"></img>
+            </div>
           </div>
         </div>
       </div>
